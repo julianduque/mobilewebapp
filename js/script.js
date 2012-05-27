5ea1f59eddca9966324e8653396ea6f6eb061ce1
Julian Duque <julianduquej@gmail.com>
2012-05-27 00:04:14 -0500

    Add helloworld HTML file with JavaScript functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-/* Author:
 
-*/
-
-
+$(document).ready(function() {
+  
+  $("#greet").click(function(e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var greeting = "Hello, " + name;
+    $("#greeting").html(greeting);
+  });
+  
+});
 
 
 
