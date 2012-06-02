5456443fb230d36c0f80e8ad0e1616c69d16e3ab
Julian Duque <julianduquej@gmail.com>
2012-06-02 15:46:52 -0500

    Add jqm

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-
 $(document).ready(function() {
   
   $("#greet").click(function(e) {
     e.preventDefault();
+    
     var name = $("#name").val();
-    var greeting = "Hello, " + name;
-    $("#greeting").html(greeting);
+    var service = "http://localhost:4567/greet";
+    $.get(service + "/" + name)
+    .success(function(greeting) {
+            $("#greeting").html(greeting);
+    });
   });
   
 });
-
-
-
